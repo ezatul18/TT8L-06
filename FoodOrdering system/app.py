df2a2a43a078427e474003448bf4e6a35cce72bb
nur fatimah <nurtimahazzahra@gmail.com>
2024-05-14 07:24:15 +0800

    divide the files

--- FoodOrdering system/app.py
+++ FoodOrdering system/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, send_file , request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("Railwayfood.html")
+    return send_file("C:\\Users\\nurti\\TT8L-06\\FoodOrdering system\\Railwayfood.html")
 
 
 
