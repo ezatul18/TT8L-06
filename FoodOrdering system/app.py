f15c1793b546a3509b51e0c1f49eac58d7b7eefa
nur fatimah <nurtimahazzahra@gmail.com>
2024-05-13 18:24:35 +0800

    make app.py

---
+++ FoodOrdering system/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("Railwayfood.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
+ 
