14dd53b69298eea49222e3790425df8370839d2a
nur fatimah <nurtimahazzahra@gmail.com>
2024-05-14 08:49:06 +0800

    new one for testing

--- Railwayfood/app.py
+++ Railwayfood/app.py
@@ -2,4 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@route
+@app.route("/home")
+def home():
+    return render_template("base.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
