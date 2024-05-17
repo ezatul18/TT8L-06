32cead12fcbb54b881331fc21fe41ebfc3528fd9
ezatul ashikin <ezatulashikin06@email.com>
2024-05-17 21:08:51 +0800

    create database

---
+++ app.py
@@ -0,0 +1,5 @@
+from mylogin import create_app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
--- mylogin/auth.py
+++ mylogin/auth.py
@@ -1,17 +1,22 @@
 # mylogin/auth.py
 
-from flask import Blueprint, render_template , redirect,url_for
+from flask import Blueprint, render_template , redirect,url_for, request
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login" , methods=['GET','POST'])
 def login():
     return render_template("login.html")
+    email = request.form.get("email")
+    password = request.form.get("password")
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up" , methods=['GET','POST'])
 def sign_up():
-    return render_template("sign_up.html")
+    username = request.form.get("username")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    return render_template("sign_up.html", methods=['GET','POST'])
 
 @auth.route("/logout")
 def logout():
