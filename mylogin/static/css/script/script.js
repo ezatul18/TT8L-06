838cb2bd5a4710fb93e455d8331128d6bac26ec7
ezatul ashikin <ezatulashikin06@email.com>
2024-06-03 21:00:53 +0800

    homepage edit

--- mylogin/static/css/script/script.js
+++ mylogin/static/css/script/script.js
@@ -22,6 +22,18 @@ const navSlide = () => {
         });
     });
 }
-
+function toggleAbout(element, transportation) {
+    // Toggle the visibility of the About button
+    var aboutButton = element.parentElement.querySelector('.about-button');
+    if (aboutButton) {
+      aboutButton.style.display = (aboutButton.style.display === 'none') ? 'block' : 'none';
+      aboutButton.textContent = 'About ' + transportation;
+    }
+  }
+  function scrollToTransportation() {
+    const transportationSection = document.getElementById('transportation');
+    transportationSection.scrollIntoView({ behavior: 'smooth' });
+  }
+  
 
 navSlide();
