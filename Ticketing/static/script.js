5424216ea4da344b414d80ec74bcc35d3a95dffa
Enath <enathjameela@gmail.com>
2024-06-04 21:59:40 +0800

    java calendar

--- Ticketing/static/script.js
+++ Ticketing/static/script.js
@@ -42,5 +42,19 @@ const dates_element = document.querySelector('.date-picker .dates');
 date_picker_element.addEventListener('click', toggleDatePicker);
 
 function toggleDatePicker (e) {
-    dates_element.classList.toggle('active');
+    console.log(e.path);
+    
+    if (!checkEventPathForClass(e.path, 'dates')) {
+        dates_element.classList.toggle('active');
+    }
+}
+
+function checkEventPathForClass (path, selector) {
+    for (let i = 0; i < path.length; i++) {
+        if (path[i].classList && path[i].classList.contains(selector)) {
+            return true;
+        }
+    }
+    
+    return false;
 }
