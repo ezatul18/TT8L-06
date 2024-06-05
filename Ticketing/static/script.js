63b8a83b3a17f525d8ce24c242bdbb4a34b2fac5
Enath <enathjameela@gmail.com>
2024-06-05 13:22:43 +0800

    adding dates

--- Ticketing/static/script.js
+++ Ticketing/static/script.js
@@ -39,6 +39,7 @@ const date_picker_element = document.querySelector('.date-picker');
 const selected_date_element = document.querySelector('.date-picker .selected-date');
 const dates_element = document.querySelector('.date-picker .dates');
 
+
 date_picker_element.addEventListener('click', toggleDatePicker);
 
 function toggleDatePicker (e) {
