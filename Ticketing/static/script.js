90c877317862f0be69639b0277c401451051b4f4
Enath <enathjameela@gmail.com>
2024-06-05 13:03:04 +0800

    arrows for calendar

--- Ticketing/static/script.js
+++ Ticketing/static/script.js
@@ -42,15 +42,13 @@ const dates_element = document.querySelector('.date-picker .dates');
 date_picker_element.addEventListener('click', toggleDatePicker);
 
 function toggleDatePicker (e) {
-    console.log(e.path);
-    
     if (!checkEventPathForClass(e.path, 'dates')) {
         dates_element.classList.toggle('active');
     }
 }
 
 function checkEventPathForClass (path, selector) {
-    for (let i = 0; i < path.length; i++) {
+    for (let i = 0; i < path; i++) {
         if (path[i].classList && path[i].classList.contains(selector)) {
             return true;
         }
