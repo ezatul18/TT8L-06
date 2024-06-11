ff3f7d744b44e12343b1b6cf8bebf3f99a6c3ac3
Enath <enathjameela@gmail.com>
2024-06-12 01:54:45 +0800

    search to booking page

--- Ticketing/app.py
+++ Ticketing/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('ets.html')
 
+@app.route('/book')
+def book():
+    return render_template('book.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
