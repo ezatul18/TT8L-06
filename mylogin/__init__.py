a58a88a17937dc7ba5dc205b9b343cdeaf9a37a6
ezatul ashikin <ezatulashikin06@email.com>
2024-06-12 18:18:12 +0800

    edit database

---
+++ mylogin/__init__.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_login import LoginManager
+from .models import connect_db
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "testtest"  
+    
+    connect_db()
+
+    app.static_folder = 'static'
+
+    
+    from .views import views
+    from .auth import auth
+
+    
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
+    
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return models.get_user_by_id(id)  
+
+    return app
+
+
+
+
+
+
+
+
+    
