e0e88ba293dd117faf122c1ce4bcb52a34cbd8c5
ezatul ashikin <ezatulashikin06@email.com>
2024-06-19 16:15:07 +0800

    add ktm ets page

--- RailwayGo/views.py
+++ RailwayGo/views.py
@@ -15,6 +15,14 @@ def about():
 def homefood():
     return render_template("home_food.html")
 
+@views.route("/ets")  
+def ets():
+    return render_template("ets.html")
+
+@views.route("/ktm")  
+def ktm():
+    return render_template("ktm.html")
+
 
 
 
